d7540a1d7fa1cfe229e10cd8c09b05fd70bbb7cb
rupeshchy10 <rupevilary1010@gmail.com>
2025-03-14 16:11:33 +0545

    Update project title to 'Filmy'; integrate Appwrite SDK for search count tracking; fix class attributes in Spinner component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
 import { useDebounce } from "react-use";
+import { updateSearchCount } from "./appwrite.js";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -47,6 +48,10 @@ const App = () => {
 				return;
 			}
 			setMovieList(data.results || []);
+
+			if(query && data.results.length > 0){
+				await updateSearchCount(query, data.results[0]);
+			}
 		} catch (error) {
 			console.log(`Error while fetching movies: ${error}`);
 			setErrorMessage(
